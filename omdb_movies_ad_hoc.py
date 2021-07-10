87ea91217d80eb4e43cc4a9a7582e9958c71e193
Vagelis Katsiotis <vageliskatsiotis@gmail.com>
2021-07-10 13:28:21 +0300

    Fixed AdHoc Movies for already fixed meta titles

--- omdb_movies_ad_hoc.py
+++ omdb_movies_ad_hoc.py
@@ -3,10 +3,9 @@ import subprocess
 import errno
 import re
 import platform
-
+import enzyme
 if (platform.system() == "Windows"):
 	import winapps
-	
 from mutagen import MutagenError
 from mutagen.mp4 import MP4
 from io import StringIO
@@ -46,7 +45,6 @@ def CheckMKVToolNix():
 
 # Rename files
 def RenameLoop(directory):
-	query = ""
 	# Loop through files
 	for root, dirs, files in os.walk(directory):
 		# Exclude searching on hidden files and folders
@@ -54,15 +52,15 @@ def RenameLoop(directory):
 		dirs[:] = [d for d in dirs if not d[0] == '.']
 		# Loop
 		for file in files:
-			# Metadata title
-			meta_title = file[:-4]
-			#get year for filename
-			year = file[-9:-5]
-			# Get file's length
-			file_length = len(file)
-			# Get file's extension
-			extension = file[file_length - 4:]
 			if (".mp4" in file or ".mkv" in file):
+				# Metadata title
+				meta_title = file[:-4]
+				#get year for filename
+				year = file[-9:-5]
+				# Get file's length
+				file_length = len(file)
+				# Get file's extension
+				extension = file[file_length - 4:]
 				# Get file's name
 				original_filename = os.path.join(root, file)
 				# Get replacemet filename
@@ -71,46 +69,57 @@ def RenameLoop(directory):
 				os.rename(original_filename, new_filename)
 				if extension == ".mp4":
 					try:
-						# New mp4 instance
-						video = MP4(new_filename)
-						# Add title to instance
-						video["\xa9nam"] = str(meta_title)
-						# Add year to instance
-						video["\xa9day"] = str(year)
-						# Empty comments if any
-						video["\251cmt"] = ""
-						# Save instance metadata to file
-						video.save()
-						# Print successfull message
-						print("Found movie " + meta_title + extension)
+						video_orig = MP4(original_filename)
+						orig_meta_title = video_orig["\xa9nam"]
+						if str(orig_meta_title[0]) == str(meta_title):
+							continue
+						else:
+							# New mp4 instance
+							video = MP4(new_filename)
+							# Add title to instance
+							video["\xa9nam"] = str(meta_title)
+							# Add year to instance
+							video["\xa9day"] = str(year)
+							# Empty comments if any
+							video["\251cmt"] = ""
+							# Save instance metadata to file
+							video.save()
+							# Print successfull message
+							print("Found movie " + meta_title + extension)
 					except MutagenError as m_error:
 						print("Metadata title for " + meta_title + " failed with error: " + m_error)
 				# Add metadata title for mkv files
 				elif extension == ".mkv":
 					try:
-						# Check OS first and MKVToolNix for Windows
-						if ((platform.system() == "Windows") and CheckMKVToolNix()):
-							mkvpropedit = r"C:\Program Files\MKVToolNix\mkvpropedit.exe"
-							subprocess.run([mkvpropedit, new_filename, '--edit', 'info', '--set', f'title={meta_title}'])
-						elif platform.system() == "Linux":
-							mkvpropedit = "/usr/bin/mkvpropedit"
-							# Check if mkvpropedit exists in linux system
-							if os.path.exists(mkvpropedit):
-								# Call mkvpropedit using subprocess to change metadata title
-								subprocess.run([mkvpropedit, new_filename, '--edit', 'info', '--set', f'title={meta_title}'], capture_output=True)
-								# Print successfull message
-								print("Found movie " + meta_title + extension)
-							else:
-								# Open a file with access mode "a"
-								file = open(os.path.expanduser("~") + "/meta_titles_not_changed.txt", "a")
-								# Append title at the end of file
-								file.write(meta_title)
-								# New line
-								file.write("\n")
-								# Close the file
-								file.close()
-								# Print message
-								print("mkvpropedit does not exist in current system. Metadata title for " + new_filename + "will not be updated")
+						with open(original_filename, 'rb') as f:
+							video_orig = enzyme.MKV(f)
+							orig_title = video_orig.info.title
+						if orig_title == meta_title:
+							continue
+						else:
+							# Check OS first and MKVToolNix for Windows
+							if ((platform.system() == "Windows") and CheckMKVToolNix()):
+								mkvpropedit = r"C:\Program Files\MKVToolNix\mkvpropedit.exe"
+								subprocess.run([mkvpropedit, new_filename, '--edit', 'info', '--set', f'title={meta_title}'])
+							elif platform.system() == "Linux":
+								mkvpropedit = "/usr/bin/mkvpropedit"
+								# Check if mkvpropedit exists in linux system
+								if os.path.exists(mkvpropedit):
+									# Call mkvpropedit using subprocess to change metadata title
+									subprocess.run([mkvpropedit, new_filename, '--edit', 'info', '--set', f'title={meta_title}'], capture_output=True)
+									# Print successfull message
+									print("Found movie " + meta_title + extension)
+								else:
+									# Open a file with access mode "a"
+									file = open(os.path.expanduser("~") + "/meta_titles_not_changed.txt", "a")
+									# Append title at the end of file
+									file.write(meta_title)
+									# New line
+									file.write("\n")
+									# Close the file
+									file.close()
+									# Print message
+									print("mkvpropedit does not exist in current system. Metadata title for " + new_filename + "will not be updated")
 					except OSError as e:
 						if e.errno != errno.EEXIST:
 							raise
